de9b8b8c286030604f2417ae1c45286000857522
yizenghui <zenghuitrue@gmail.com>
2018-05-20 15:34:01 +0800

    通过审核了，回头搞事

--- example.go
+++ example.go
@@ -11,9 +11,9 @@ type DemoItem struct {
 func RssDemoList() []DemoItem {
 	var list = []DemoItem{
 		DemoItem{`月光博客`, `http://feed.williamlong.info/`, `Rss`},
-		// DemoItem{`知乎每日精选`, `https://www.zhihu.com/rss`, `Rss`},
-		// DemoItem{`cnbeta`, `https://www.cnbeta.com/backend.php`, `Rss`},
-		// DemoItem{`国内新闻-腾讯`, `http://news.qq.com/newsgn/rss_newsgn.xml`, `Rss`},
+		DemoItem{`知乎每日精选`, `https://www.zhihu.com/rss`, `Rss`},
+		DemoItem{`cnbeta`, `https://www.cnbeta.com/backend.php`, `Rss`},
+		DemoItem{`国内新闻-腾讯`, `http://news.qq.com/newsgn/rss_newsgn.xml`, `Rss`},
 	}
 	return list
 }
@@ -21,8 +21,8 @@ func RssDemoList() []DemoItem {
 //ArticleDemoList 文章示例列表
 func ArticleDemoList() []DemoItem {
 	var list = []DemoItem{
-		DemoItem{`laravel5.6`, `http://laravelacademy.org/laravel-docs-5_6`, `Article`},
-		// DemoItem{`wechatRank.com`, `https://wechatrank.com/getlist`, `Article`},
+		// DemoItem{`laravel5.6`, `http://laravelacademy.org/laravel-docs-5_6`, `Article`},
+		DemoItem{`wechatRank.com`, `https://wechatrank.com/getlist`, `Article`},
 	}
 	return list
 }
@@ -30,12 +30,12 @@ func ArticleDemoList() []DemoItem {
 //BookDemoList 小说示例列表
 func BookDemoList() []DemoItem {
 	var list = []DemoItem{
-		DemoItem{`laravel5.6`, `http://laravelacademy.org/laravel-docs-5_6`, `Book`},
-		// DemoItem{`点道为止`, `http://book.zongheng.com/showchapter/730066.html`, `Book`},
-		// DemoItem{`修罗武神`, `http://www.17k.com/list/493239.html`, `Book`},
-		// DemoItem{`万古仙穹`, `https://www.cangqionglongqi.com/wanguxianzuo/`, `Book`},
-		// DemoItem{`斗罗大陆`, `http://www.biquge.info/10_10218/`, `Book`},
-		// DemoItem{`圣墟`, `http://www.biqiuge.com/book/4772/`, `Book`},
+		// DemoItem{`laravel5.6`, `http://laravelacademy.org/laravel-docs-5_6`, `Book`},
+		DemoItem{`点道为止`, `http://book.zongheng.com/showchapter/730066.html`, `Book`},
+		DemoItem{`修罗武神`, `http://www.17k.com/list/493239.html`, `Book`},
+		DemoItem{`万古仙穹`, `https://www.cangqionglongqi.com/wanguxianzuo/`, `Book`},
+		DemoItem{`斗罗大陆`, `http://www.biquge.info/10_10218/`, `Book`},
+		DemoItem{`圣墟`, `http://www.biqiuge.com/book/4772/`, `Book`},
 	}
 	return list
 }
