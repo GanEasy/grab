54647833d934d504437fee2f28831368bbab0c12
yizenghui <245561237@qq.com>
2019-07-18 15:23:49 +0800

    最大搜索结果50条

--- db/post.go
+++ db/post.go
@@ -34,7 +34,7 @@ func (post *Post) GetPostByID(id int) {
 
 //GetPostsByName  通过名字获得查询记录
 func (post *Post) GetPostsByName(name string) (posts []Post) {
-	DB().Where("name LIKE ?", "%"+name+"%").Find(&posts)
+	DB().Limit(50).Where("name LIKE ?", "%"+name+"%").Find(&posts)
 	// DB().Where("name = ?", name).Find(&posts)
 	return
 }
