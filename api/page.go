e8d5661ea641b468a2cfc7cf80e74083482c4cc5
yizenghui <121258121@qq.com>
2020-09-17 16:03:26 +0800

    save

--- api/page.go
+++ api/page.go
@@ -880,7 +880,7 @@ func GetGuideExploreLinks() []Link {
 func GetGuideExploreLinksAndJumpVipApp() []Link {
 	var links = []Link{
 		Link{
-			Title: `》》》进入VIP通道笔趣阁Pro，搜索“112233”`,
+			Title: `老铁进VIP通道搜“112233”`,
 			Icon:  `notice`,
 			Type:  `jumpapp`,
 			Image: ``,
