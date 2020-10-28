24d4b8a4cffe458df77b46d58cd40d28b5895142
yizenghui <121258121@qq.com>
2020-10-28 15:23:38 +0800

    恢复广告

--- serve/grabserve.go
+++ serve/grabserve.go
@@ -271,7 +271,7 @@ drive sup: qidian,zongheng,17k,luoqiu,booktxt,bxwx,uxiaoshuo,soe8,manhwa,r2hm,xb
 		reader := grab.GetReader(drive)
 		list, _ := reader.GetInfo(urlStr)
 		if len(list.Contents)<10{
-			list.Contents = append(list.Contents,` `,`------ `,` `,` `,` `,` `,` `,` `,` `)
+			list.Contents = append(list.Contents,`---------- `,`---------- `,`---------`,`---------`,`--------`,`--------`,`-------`,`-------`,`------`,`------`,`-----`,`-----`,`----`,`----`,`---`,`---`,`--`,`--`,`-`,`-`)
 		}
 		return c.JSON(http.StatusOK, list)
 	})
