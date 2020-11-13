dedcc3bd090e90a51862fe0a125900557ea23483
yizenghui <121258121@qq.com>
2020-11-13 08:54:14 +0800

    ss

--- api/activity.go
+++ api/activity.go
@@ -74,7 +74,7 @@ func GetActivities(c echo.Context) error {
 	}
 	var req = c.Request()
 	if strings.Contains(req.Referer(), `wx8664d56a896e375b`) { // 免版权图 退场中
-		level = 2
+		// level = 2
 	}
 	var rows = cpi.GetActivities()
 	if len(rows) > 0 {
