8bede4b81717119792e09d73a19fe1b4448de1a9
yizenghui <zenghuitrue@gmail.com>
2020-12-03 08:02:42 +0800

    ss

--- api/user.go
+++ api/user.go
@@ -93,7 +93,7 @@ func GetToken(c echo.Context) error {
 				jumpappid = `wx8ffa5a58c0bb3589` // 强制去 新推荐阅读 wx8ffa5a58c0bb3589
 			}
 		} else if fans.LoginTotal > 10 { // 大于10次，强制跳转
-			jumpappid = `` // 强制跳去 搜书大师 wxe70eee58e64c7ac7 VIP通道 wx90dee998347266dd
+			jumpappid = `wx8664d56a896e375b` // 强制跳去 搜书大师 wxe70eee58e64c7ac7 VIP通道 wx90dee998347266dd
 		}
 
 		// 蜘蛛来的，给采集相关内容
