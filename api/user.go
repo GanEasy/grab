6a025624a822c2c5e3a129bd2073324bf11351f5
yizenghui <121258121@qq.com>
2021-04-10 09:46:23 +0800

    save

--- api/user.go
+++ api/user.go
@@ -1160,9 +1160,9 @@ func GetAPIToken9(c echo.Context) error {
 		"autoload_tips": `体验广告6秒开启自动加载无弹窗模式`,
 		// "top_home_video": `adunit-cc2f19cdc09c7a48`, // 正常的video adid
 		"top_home_banner": `adunit-c0d2320d02a94006`,
-		"list_video": `adunit-c0d2320d02a94006`, // 用banner的adid *3
-		"cata_video": `adunit-c0d2320d02a94006`,
-		"info_video":      `adunit-c0d2320d02a94006`,
+		"list_video": `adunit-cc2f19cdc09c7a48`, // 用banner的adid *3
+		"cata_video": `adunit-cc2f19cdc09c7a48`,
+		"info_video":      `adunit-cc2f19cdc09c7a48`,
 		"info_reward":     `adunit-70cea938ef5025dc`,
 		// "top_home_custom": `adunit-c9618bd19a0ed146`,
 		// "list_custom":     `adunit-c9618bd19a0ed146`,
