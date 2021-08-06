36991f53e20e9ed3edcac0b18df160753f7947e9
yizenghui <zenghuitrue@gmail.com>
2021-08-06 23:26:45 +0800

    sasave

--- reader/xbiquge.go
+++ reader/xbiquge.go
@@ -63,7 +63,7 @@ func (r XbiqugeReader) GetList(urlStr string) (list Catalog, err error) {
 	var needLinks []Link
 	var state bool
 	for _, l := range links {
-		l.URL, state = JaccardMateGetURL(l.URL, `http://www.xbiquge.la/39/39551/`, `http://www.xbiquge.la/27/27807/`, ``)
+		l.URL, state = JaccardMateGetURL(l.URL, `https://www.xbiquge.la/39/39551/`, `https://www.xbiquge.la/27/27807/`, ``)
 		if state {
 			l.Title = FindString(`(?P<title>(.)+)`, l.Title, "title")
 			needLinks = append(needLinks, l)
@@ -92,6 +92,10 @@ func (r XbiqugeReader) Search(keyword string) (list Catalog, err error) {
 
 // GetCatalog 获取章节列表
 func (r XbiqugeReader) GetCatalog(urlStr string) (list Catalog, err error) {
+
+	reg := regexp.MustCompile(`http://`)
+
+	urlStr = reg.ReplaceAllString(urlStr, "https://")
 
 	err = CheckStrIsLink(urlStr)
 	if err != nil {
@@ -125,7 +129,7 @@ func (r XbiqugeReader) GetCatalog(urlStr string) (list Catalog, err error) {
 	var needLinks []Link
 	var state bool
 	for _, l := range links {
-		l.URL, state = JaccardMateGetURL(l.URL, `http://www.xbiquge.la/39/39551/18263995.html`, `http://www.xbiquge.la/7/7004/3246402.html`, ``)
+		l.URL, state = JaccardMateGetURL(l.URL, `https://www.xbiquge.la/39/39551/18263995.html`, `https://www.xbiquge.la/7/7004/3246402.html`, ``)
 		if state {
 			needLinks = append(needLinks, l)
 		}
